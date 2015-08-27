94768e97b4064f3b883b163a192e55d8a173c5a0
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-27 11:44:03 -0600

    angular route added to partial

--- posts-workshop/public/app.js
+++ posts-workshop/public/app.js
@@ -1,5 +1,14 @@
-var app = angular.module('posts-workshop', [])
+var app = angular.module('posts-workshop', ['ngRoute'])
 
 app.controller('ApplicationController', ['$scope', function($scope) {
   $scope.application= 'Hello!';
 }]);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/posts.html',
+        controller: 'ApplicationController'
+      })
+  }]);
