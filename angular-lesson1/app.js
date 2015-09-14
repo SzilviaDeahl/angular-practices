899937cceb6c3a5177767b7f9bc354fb3668bf69
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-14 16:32:31 -0600

    seconds in century exercise finished, number value formatted with built-in filter

--- angular-lesson1/app.js
+++ angular-lesson1/app.js
@@ -1,4 +1,11 @@
-var app = angular.module("firstApp", []);
-app.controller("MyFirstController", function($scope){
-  $scope.name = "Severus Snape";
+// var app = angular.module("firstApp", []);
+// app.controller("MyFirstController", function($scope){
+//   $scope.name = "Severus Snape";
+// })
+
+
+var app = angular.module('firstApp', []);
+app.controller('ExercisesController', function ($scope) {
+  $scope.FavColor = 'Blue';
+  $scope.secondsInACentury = 60 * 60 * 24 * 365 * 100;
 })
