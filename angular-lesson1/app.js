bebe5e4d80b2aa9e35cab24e9eb7279945cf624e
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-14 16:37:15 -0600

    able to display current date in angular

--- angular-lesson1/app.js
+++ angular-lesson1/app.js
@@ -8,4 +8,5 @@ var app = angular.module('firstApp', []);
 app.controller('ExercisesController', function ($scope) {
   $scope.FavColor = 'Blue';
   $scope.secondsInACentury = 60 * 60 * 24 * 365 * 100;
+  $scope.rightNow = new Date();
 })
